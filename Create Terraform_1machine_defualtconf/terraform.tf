2b92d2254522ff2169664ea9da92448be9f987dc
Ebin Davis <30700182+ebindavis17@users.noreply.github.com>
2017-12-12 09:13:13 +0530

    Create terraform.tf

---
+++ Create Terraform_1machine_defualtconf/terraform.tf
@@ -0,0 +1,23 @@
+#Provider file
+
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+ } 
+  
+#creating EC2 instance
+
+resource "aws_instance" "ec2" {
+  ami           = "${var.ami_1}"
+  instance_type = "t2.micro"
+  key_name = "new"
+    tags = {
+        Name = "Instance-1
+}
+connection {
+type = "ssh"
+user = "ubuntu"
+private_key = "${file("/path/to/keyfile")}"
+}
+}
