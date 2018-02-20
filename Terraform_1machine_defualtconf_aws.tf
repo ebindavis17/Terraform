3a5b4dd28339f166ac623fc3d88d2b47e52aa1d1
Ebin Davis <30700182+ebindavis17@users.noreply.github.com>
2018-02-21 01:32:23 +0530

    Update Terraform_1machine_defualtconf_aws.tf

--- Terraform_1machine_defualtconf_aws.tf
+++ Terraform_1machine_defualtconf_aws.tf
@@ -11,14 +11,14 @@ provider "aws" {
 resource "aws_instance" "ec2" {
   ami           = "${var.ami_1}"
   instance_type = "t2.micro"
-  key_name = "new"
+  key_name = "new"              #key which you want to add - which already in the key-pair list
     tags = {
         Name = "Instance-1
 }
 connection {
 type = "ssh"
 user = "ubuntu"
-private_key = "${file("/path/to/keyfile")}"
+private_key = "${file("/path/to/keyfile")}"  #key_pair to be added to thr instance
 }
 }
 
